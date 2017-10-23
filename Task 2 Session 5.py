32ea669282d980940b835a74e3bde705236f68d3
Stefanos1312 <32456667+Stefanos1312@users.noreply.github.com>
2017-10-23 14:57:58 +0100

    Add files via upload

---
+++ Task 2 Session 5.py
@@ -0,0 +1,5 @@
+myage=int(input("how old are you"))
+print ("hi there,you are",myage,"years old")
+
+
+
---
+++ Task 3 Session 5.py
@@ -0,0 +1,4 @@
+number1 = int(input("enter first number"))
+number2 = int(input("Enter second number"))
+result = (number1 + number2)
+print("the result is",result)
